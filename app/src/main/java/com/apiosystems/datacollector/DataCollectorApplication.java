ebca576106b951cd50c7d63742ed0aee9fdc85b5
akshayrajkore <akshayrajkore@gmail.com>
2015-04-22 16:43:57 -0400

    Added the UI components

---
+++ app/src/main/java/com/apiosystems/datacollector/DataCollectorApplication.java
@@ -0,0 +1,10 @@
+package com.apiosystems.datacollector;
+
+import android.app.Application;
+
+/**
+ * Created by Akshayraj on 4/22/15.
+ */
+public class DataCollectorApplication extends Application {
+
+}
---
+++ app/src/main/java/com/apiosystems/datacollector/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.apiosystems.datacollector.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import datacollector.apiosystems.com.datacollector.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_activity);
+    }
+}
